759c182cf50e5db01995aa2a24707344c315854e
phuong98-sys <phuong98mta@gmail.com>
2021-04-29 17:37:31 +0700

    29/4

---
+++ GmailWebAPI/GmailWebAPI/MVC4/Controllers/AuthCallbackController.cs
@@ -0,0 +1,13 @@
+﻿using GmailWebAPI.MVC4;
+using Google.Apis.Sample.MVC4;
+
+namespace Google.Apis.Sample.MVC4.Controllers
+{
+    public class AuthCallbackController : Google.Apis.Auth.OAuth2.Mvc.Controllers.AuthCallbackController
+    {
+        protected override Google.Apis.Auth.OAuth2.Mvc.FlowMetadata FlowData
+        {
+            get { return new AppFlowMetadata(); }
+        }
+    }
+}
