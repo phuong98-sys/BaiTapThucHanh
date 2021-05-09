d4e97a092170449ca10a95b56445fcc248852035
phuong98-sys <Phuongred98@gmail.com>
2021-05-10 03:51:57 +0700

    cm 10/5 rang
    rang 10/5

--- GmailWebAPI/DemoGmailWithOAuthAPI/Controllers/GmailAPIController.cs
+++ GmailWebAPI/DemoGmailWithOAuthAPI/Controllers/GmailAPIController.cs
@@ -38,11 +38,20 @@ using System.IO;
 using System.Web.Security;
 
 using System.Net.Http;
+using System.Net;
+using System.Web.Script.Serialization;
+using MimeKit;
+using Google.Apis.Auth.OAuth2.Flows;
+using Google.Apis.Auth.OAuth2;
+//https://www.youtube.com/watch?v=RKBl33cklhc
 namespace DemoGmailWithOAuthAPI.Controllers
 {
     public class GmailAPIController : Controller
     {
         // GET: GmailAPI
+        public static string name;
+        public static string token1;
+        public static string token2;
         public ActionResult Index()
         {
             return View();
@@ -53,30 +62,45 @@ namespace DemoGmailWithOAuthAPI.Controllers
         }
         public void SendEmail(EmailContent ec)
         {
-            string subject = ec.subject;
-            string body = ec.body;
-            string to = ec.to;
-            MailMessage mm = new MailMessage();
-            mm.From = new MailAddress("phuongred98@gmail.com");
-            mm.To.Add(to);
-            mm.Subject = subject;
-            mm.Body = body;
-            mm.IsBodyHtml = false;
-            SmtpClient smtp = new SmtpClient("smtp.gmail.com");
-            smtp.UseDefaultCredentials = true;
-            smtp.Port = 587;
-            smtp.EnableSsl = true;
-            smtp.Credentials = new System.Net.NetworkCredential("phuongred98@gmail.com", "Baoiba98@");
-            smtp.Send(mm);
-           
+            string id= SendMail(name, name, "test", "teest");
+            if(id!=null)
+            {
+                ViewBag.s = "thanh cong";
+            }
+            
+            //string to = ec.to;
+            //MailMessage mm = new MailMessage(name,to);
+            //string subject = ec.subject;
+            //string body = ec.body;
+            //mm.Subject = subject;
+            //mm.Body = body;
+            //mm.IsBodyHtml = false;
+            //SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            //smtp.Timeout = 1000000;
+            //smtp.EnableSsl = true;
+            //smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            ////NetworkCredential nc = new NetworkCredential()
+            ////mm.From = new MailAddress(name);
+            ////mm.To.Add(to);
+
+            ////SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+            //smtp.UseDefaultCredentials = true;
+            //smtp.Port = 587;
+            //smtp.EnableSsl = true;
+            //smtp.Credentials = new System.Net.NetworkCredential("phuongred98@gmail.com", "Baoiba98@");
+            //smtp.Send(mm);
+
         }
-        #region to Fetch Response from Gmail API
+        #region to Fetch Response from Gmail API 
+        // lay ma uy quyen
         public ActionResult Code(string state, string code, string scope)
         {
             string GoogleWebAppClientID = WebConfigurationManager.AppSettings["GoogleWebAppClientID"];
             string GoogleWebAppClientSecret = WebConfigurationManager.AppSettings["GoogleWebAppClientSecret"];
             string RedirectUrl = WebConfigurationManager.AppSettings["RedirectUrl"];
+                // AccessToken:
             string Token = CreateOauthTokenForGmail(code, GoogleWebAppClientID, GoogleWebAppClientSecret, RedirectUrl);
+            token1 = Token;
             Session["Token"] = Token;
             return RedirectToAction("DisplayEmail");
         }
@@ -122,8 +146,12 @@ namespace DemoGmailWithOAuthAPI.Controllers
         //    }
         //}
         //
+
+        // sen moi
+
         #endregion
         #region to Create AccessToken by using this Parameters
+        // Lay ma truy cap
         public string CreateOauthTokenForGmail(string code, string GoogleWebAppClientID, string GoogleWebAppClientSecret, string RedirectUrl)
         {
             RequestParameters requestParameters = new RequestParameters()
@@ -145,15 +173,74 @@ namespace DemoGmailWithOAuthAPI.Controllers
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
 
-                StringContent content = new StringContent(inputJson, Encoding.UTF8, "application/json");
+               StringContent content = new StringContent(inputJson, Encoding.UTF8, "application/json");
                 respone = client.PostAsync(requestURI, content).Result;
 
                 if(respone.IsSuccessStatusCode)
                 {
-                    
+                    // chuyen doi chuoi tra ve
+                    string r = respone.Content.ReadAsStringAsync().ToString();
+                    var t = JsonConvert.DeserializeObject<OAuthTokenViewModel>(r);
                     ResponseString = JsonConvert.DeserializeObject(respone.Content.ReadAsStringAsync().Result).ToString();
-                    var result = JsonConvert.DeserializeObject<OAuthTokenViewModel>(ResponseString);
-                    ResponseString = result.Access_token.ToString();
+                    var result = JsonConvert.DeserializeObject<OAuthTokenViewModel>(ResponseString); // gan cho OAuthTokenViewModel
+                    ResponseString = result.Access_token.ToString(); // access Token
+                 
+
+                    // Lay thông tin user
+                    try
+
+                    {
+
+                        //HttpClient client2 = new HttpClient();
+
+                        var url = "https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=" + ResponseString;
+                        //WebRequest request = WebRequest.Create(url);
+                        //request.Credentials = CredentialCache.DefaultCredentials;
+                        //WebResponse response = request.GetResponse();
+                        //Stream dataStream = response.GetResponseStream();
+                        //StreamReader reader = new StreamReader(dataStream);
+                        //string responseFromServer = reader.ReadToEnd();
+                        //reader.Close();
+                        //response.Close();
+                        //JavaScriptSerializer js = new JavaScriptSerializer();
+                        //GoogleUserOutputData userinfo = js.Deserialize<GoogleUserOutputData>(responseFromServer);
+                        //imgprofile.ImageUrl = userinfo.picture;
+                        //lblid.Text = userinfo.id;
+                        //lblgender.Text = userinfo.gender;
+                        //lbllocale.Text = userinfo.locale;
+                        //lblname.Text = userinfo.name;
+                        //var name = userinfo.email;                    
+                        client.CancelPendingRequests();
+
+                        HttpResponseMessage output = client.GetAsync(url).Result;
+                        GoogleUserOutputData serStatus = new GoogleUserOutputData();
+
+                        if (output.IsSuccessStatusCode)
+
+                        {
+
+                            string outputData = output.Content.ReadAsStringAsync().Result;
+
+                            serStatus = JsonConvert.DeserializeObject<GoogleUserOutputData>(outputData);
+                                name = serStatus.email;
+
+                        }
+                        else
+                        {
+                            ViewBag.test = "no";
+                        }
+
+                    }
+
+                    catch (Exception ex)
+
+                    {
+
+                        //catching the exception
+
+                    }
+
+                    //return View(serStatus);
 
                 }
                 return ResponseString;
@@ -161,21 +248,23 @@ namespace DemoGmailWithOAuthAPI.Controllers
         }
         #endregion
         #region to Display Emails
+        // get inboxes of gmail
         public async Task<ActionResult> DisplayEmail()
         {
             HttpClient client = new HttpClient();
             Root rootObj = new Root();
+            // Gui ma truy cap cho gmail api
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(scheme: "Bearer",
                 parameter: Session["Token"].ToString());
             HttpResponseMessage responseMessage = await client.GetAsync("https://mail.google.com/mail/feed/atom");
             if(responseMessage.IsSuccessStatusCode)
             {
-                var data = responseMessage.Content;
-                var responseData = responseMessage.Content.ReadAsStringAsync().Result;
+                var data = responseMessage.Content; // du lieu tu gmail tra ve
+                var responseData = responseMessage.Content.ReadAsStringAsync().Result;// doc noi dung du lieu
                 XmlDocument doc = new XmlDocument();
-                doc.LoadXml(@responseData);
-                string json = JsonConvert.SerializeXmlNode(doc);
-                rootObj = JsonConvert.DeserializeObject<Root>(json);
+                doc.LoadXml(@responseData);//
+                string json = JsonConvert.SerializeXmlNode(doc);//chuyen du lieu xml thanh json
+                rootObj = JsonConvert.DeserializeObject<Root>(json); // gan du lieu gmail cho doi tuong nay
             }
             return View(rootObj);
         }
@@ -217,7 +306,79 @@ namespace DemoGmailWithOAuthAPI.Controllers
         //        }
 
         //}
-        private static string Base64UrlEncode(string input)
+        public string SendMail(string fromAddress, string toEmail, string subject, string body)
+        {
+            //Construct the message
+            var mailMessage = new System.Net.Mail.MailMessage(fromAddress, toEmail, subject, body);
+            mailMessage.ReplyToList.Add(new MailAddress(fromAddress));
+
+            //Specify whether the body is HTML
+            mailMessage.IsBodyHtml = true;
+
+            //Convert to MimeMessage
+            MimeMessage message = MimeMessage.CreateFromMailMessage(mailMessage);
+            var rawMessage = message.ToString();
+
+            var flow = new GoogleAuthorizationCodeFlow(new GoogleAuthorizationCodeFlow.Initializer
+            {
+                ClientSecrets = new ClientSecrets
+                {
+                    ClientId = "369518912260-5g5delg9bh600r6amuqsvmc0n995f2b7.apps.googleusercontent.com",
+                    ClientSecret = "XA4yC-8Oj6pMWhdJS5CS7ZZH"
+                },
+                Scopes = new[] { GmailService.Scope.GmailCompose }
+            });
+
+            var token = new Google.Apis.Auth.OAuth2.Responses.TokenResponse()
+            {
+                AccessToken = token1,
+                //RefreshToken =
+            };
+
+            //In my case the username is the same as the fromAddress
+            var gmail = new GmailService(new Google.Apis.Services.BaseClientService.Initializer()
+            {
+                ApplicationName = "App Name",
+                HttpClientInitializer = new UserCredential(flow, fromAddress, token)
+            });
+
+            var result = gmail.Users.Messages.Send(new Message
+            {
+                Raw = Base64UrlEncode(rawMessage)
+            }, "me").Execute();
+            return result.Id;
+        }
+    
+    /// <summary>
+    /// Converts input string into a URL safe Base64 encoded string. Method thanks to Jason Pettys.
+    /// http://jason.pettys.name/2014/10/27/sending-email-with-the-gmail-api-in-net-c/
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
+    //public void SendIt()
+    //{
+    //    var msg = new AE.Net.Mail.MailMessage
+    //    {
+    //        Subject = "Your Subject",
+    //        Body = "Hello, World, from Gmail API!",
+    //        From = new MailAddress("[you]@gmail.com")
+    //    };
+    //    msg.To.Add(new MailAddress("yourbuddy@gmail.com"));
+    //    msg.ReplyTo.Add(msg.From); // Bounces without this!!
+    //    var msgStr = new StringWriter();
+    //    msg.Save(msgStr);
+
+    //    // Context is a separate bit of code that provides OAuth context;
+    //    // your construction of GmailService will be different from mine.
+    //    var gmail = new GmailService(Context.GoogleOAuthInitializer);
+    //    var result = gmail.Users.Messages.Send(new Message
+    //    {
+    //        Raw = Base64UrlEncode(msgStr.ToString())
+    //    }, "me").Execute();
+    //    Console.WriteLine("Message ID {0} sent.", result.Id);
+    //}
+
+    private static string Base64UrlEncode(string input)
         {
             var inputBytes = System.Text.Encoding.UTF8.GetBytes(input);
             // Special "url-safe" base64 encode.
--- GmailWebAPI/DemoGmailWithOAuthAPI/Models/OAuthTokenViewModel.cs
+++ GmailWebAPI/DemoGmailWithOAuthAPI/Models/OAuthTokenViewModel.cs
@@ -8,9 +8,12 @@ namespace DemoGmailWithOAuthAPI.Models
     public class OAuthTokenViewModel
     {
         public string Access_token { get; set; }
+        public string Refresh_token { get; set; }
         public int Expires_in { get; set; }
         public string Scope { get; set; }
         public string Token_type { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
 
     }
 }
